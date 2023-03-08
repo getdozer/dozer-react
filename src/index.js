e8874f7c0ea0be95b99772931346d9a5f0e17321
Karolis Gudiškis <karolisgud@gmail.com>
2023-03-08 19:15:31 +0200

    Implement new hooks

--- src/index.js
+++ src/index.js
@@ -1,48 +1,90 @@
 import { useEffect, useState } from "react";
-import { DozerClient } from "@getdozer/dozer-js/src/client";
+import { ApiClient } from "@getdozer/dozer-js/src/client";
 import { RecordMapper } from "@getdozer/dozer-js/src/helper";
 import { OperationType } from "@getdozer/dozer-js/generated/protos/types_pb";
 
-const useGrpcData = (endpoint) => {
-  const [fields, setFields] = useState([])
-
-  const [state, setState] = useState({ records: [] });
-
+// TODO: Refactor this to useStreamWithInitialQuery
+// const useGrpcData = (endpoint) => {
+//   const [fields, setFields] = useState([])
+//   const [state, setState] = useState({ records: [] });
+//
+//   let client = new DozerClient(endpoint);
+//   useEffect(() => {
+//     client.query().then((response) => {
+//       let mapper = new RecordMapper(response.getFieldsList());
+//       setFields(response.getFieldsList());
+//       setState({records: response.getRecordsList().map(v => mapper.mapRecord(v.getRecord().getValuesList()))});
+//     });
+//   }, [])
+//
+//   useEffect(() => {
+//     if (fields.length > 0) {
+//       client.getFields().then(fieldsResponse => {
+//         let fields = fieldsResponse.getFieldsList();
+//         let mapper = new RecordMapper(fieldsResponse.getFieldsList());
+//         let primaryIndexList = fieldsResponse.getPrimaryIndexList();
+//         let primaryIndexKeys = primaryIndexList.map(index => fields[index].getName());
+//
+//         let stream = client.onEvent();
+//         stream.on('data', data => {
+//           if (data.getTyp() === OperationType.UPDATE) {
+//             let oldValue = mapper.mapRecord(data.getOld().getValuesList());
+//             let records = state.records;
+//             let existingIndex = records.findIndex(v => primaryIndexKeys.every(k => v[k] === oldValue[k]))
+//
+//             if (existingIndex > -1) {
+//               records[existingIndex] = mapper.mapRecord(data.getNew().getValuesList());
+//               setState({ records: [...records] });
+//             }
+//           }
+//         });
+//       })
+//     }
+//   }, [fields]);
+//
+//   return [state.records, fields];
+// }
+//
+const useCount = (endpoint) => {
+  const [count, setCount] = useState(0)
   let client = new DozerClient(endpoint);
   useEffect(() => {
-    client.query().then((response) => {
-      let mapper = new RecordMapper(response.getFieldsList());
-      setFields(response.getFieldsList());
-      setState({records: response.getRecordsList().map(v => mapper.mapRecord(v.getRecord().getValuesList()))});
+    client.count().then((response) => {
+      setCount(response.getCount)
     });
   }, [])
 
+  return [count];
+};
+
+const useQueryCommon = (endpoint) => {
+  const [state, setState] = useState({ records: [], fields: [] });
+
+  let client = new ApiClient(endpoint);
+  useEffect(() => {
+    client.query().then(([fields, records]) => {
+      setState({records, fields});
+    });
+  }, [])
+
+  return state;
+};
+
+const useOnEvent = (endpoint, cb) => {
+  const [fields, setFields] = useState([])
+  let client = new ApiClient(endpoint);
+  client.getFields().then(response => {
+    setFields(response.getFieldsList());
+  });
+
   useEffect(() => {
     if (fields.length > 0) {
-      client.getFields().then(fieldsResponse => {
-        let fields = fieldsResponse.getFieldsList();
-        let mapper = new RecordMapper(fieldsResponse.getFieldsList());
-        let primaryIndexList = fieldsResponse.getPrimaryIndexList();
-        let primaryIndexKeys = primaryIndexList.map(index => fields[index].getName());
-
-        let stream = client.onEvent();
-        stream.on('data', data => {
-          if (data.getTyp() === OperationType.UPDATE) {
-            let oldValue = mapper.mapRecord(data.getOld().getValuesList());
-            let records = state.records;
-            let existingIndex = records.findIndex(v => primaryIndexKeys.every(k => v[k] === oldValue[k]))
-
-            if (existingIndex > -1) {
-              records[existingIndex] = mapper.mapRecord(data.getNew().getValuesList());
-              setState({ records: [...records] });
-            }
-          }
-        });
-      })
+      let stream = client.onEvent();
+      stream.on('data', cb);
     }
   }, [fields]);
 
-  return [state.records, fields];
-}
+  return [fields];
+};
 
-export { useGrpcData }
+export { useQueryCommon, useCount, useOnEvent }
